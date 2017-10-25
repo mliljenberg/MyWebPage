f77abe1cbdbe8679e0f936f13f2948b933c6f711
mliljenberg <mliljenberg@gmail.com>
2017-10-24 22:03:52 -0500

    added analytics

--- app/containers/HomePage/TextConst.js
+++ app/containers/HomePage/TextConst.js
@@ -22,12 +22,6 @@ export const CV_LIST = [{
   src: 'file path',
   orient: true,
 },
-  {
-    header: 'företag 4',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    src: 'file path',
-    orient: true,
-  },
 ];
 
 export const COMP_LIST = [{ header: 'test',
